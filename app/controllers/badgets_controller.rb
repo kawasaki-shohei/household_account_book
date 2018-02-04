de51992a55cbab99403e45f55b4690cae41e0518
shohei <shoheimoment@gmail.com>
2018-02-04 22:01:57 +0900

    BadgetsController#create

--- app/controllers/badgets_controller.rb
+++ app/controllers/badgets_controller.rb
@@ -1,11 +1,22 @@
 class BadgetsController < ApplicationController
   before_action :set_all_categories, only:[:index, :new]
+
   def index
-    @categories = Category.all
   end
 
   def new
     @badget = current_user.badgets.new
+  end
+
+  def create
+    @badget = current_user.badgets.new(badget_params)
+    if @badget.save
+      category = Category.find(@badget.category_id)
+      redirect_to badgets_path, notice: "#{category.kind}の予算を#{@badget.amount}円に設定しました"
+    else
+      set_all_categories
+      render 'new'
+    end
   end
 
   def edit
@@ -14,5 +25,9 @@ class BadgetsController < ApplicationController
 private
   def set_all_categories
     @categories = Category.all
+  end
+
+  def badget_params
+    params.require(:badget).permit(:category_id, :amount)
   end
 end
