5d1c37cad40cbbb6084e5107bb9b5f1ec3090334
shohei <shoheimoment@gmail.com>
2018-02-04 21:19:49 +0900

    badgetsController#new

--- app/controllers/badgets_controller.rb
+++ app/controllers/badgets_controller.rb
@@ -1,12 +1,18 @@
 class BadgetsController < ApplicationController
+  before_action :all_categories, only:[:index, :new]
   def index
     @categories = Category.all
   end
 
   def new
-    
+    @badget = current_user.badget.new
   end
 
   def edit
+  end
+
+private
+  def set all_categories
+    @categories = Category.all
   end
 end
