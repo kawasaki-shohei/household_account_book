77c3e1d2c84bb07dd0baf5ce2f89fdbdf33303ad
shohei <shoheimoment@gmail.com>
2018-02-06 19:37:46 +0900

    set badget#update contrller and change the view badget#index so that edit badget individually

--- app/controllers/badgets_controller.rb
+++ app/controllers/badgets_controller.rb
@@ -1,5 +1,6 @@
 class BadgetsController < ApplicationController
   before_action :set_all_categories, only:[:index, :new, :edit]
+  before_action :set_badget, only: [:edit, :update]
 
   def index
   end
@@ -23,6 +24,15 @@ class BadgetsController < ApplicationController
     @badget = Badget.find(params[:id])
   end
 
+  def update
+    if @badget.update(badget_params)
+      redirect_to badgets_path
+    else
+      set_all_categories
+      render 'edit'
+    end
+  end
+
 private
   def set_all_categories
     @categories = Category.all
@@ -30,5 +40,9 @@ private
 
   def badget_params
     params.require(:badget).permit(:category_id, :amount)
+  end
+
+  def set_badget
+    @badget = Badget.find(params[:id])
   end
 end
--- app/models/badget.rb
+++ app/models/badget.rb
@@ -5,9 +5,11 @@ class Badget < ApplicationRecord
   validate :check_one_category
 
   def check_one_category
-    check = Badget.find_by(user_id: user_id, category_id: category_id)
-    if check.present?
-      errors[:base] << "同じカテゴリーに２つの予算を設定できません。予算を編集してください。"
+    if action_name == "create"
+      check = Badget.find_by(user_id: user_id, category_id: category_id)
+      if check.present?
+        errors[:base] << "同じカテゴリーに２つの予算を設定できません。予算を編集してください。"
+      end
     end
   end
 
