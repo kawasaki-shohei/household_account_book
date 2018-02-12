0721cbab03ecfddc3b4388a04a6bab518c389a44
shohei <shoheimoment@gmail.com>
2018-02-12 23:01:56 +0900

    could show which percent for 2 people

--- app/helpers/expenses_helper.rb
+++ app/helpers/expenses_helper.rb
@@ -1,2 +1,11 @@
 module ExpensesHelper
+  def percent_selection
+    selection = { '半分' => 0.5, '3分の1' => 0.33, '3分の2' => 0.66, '相手100%' => 0.0 }
+    return selection
+  end
+
+  def which_percent(val)
+    percent_selection.key(val)
+  end
+
 end
