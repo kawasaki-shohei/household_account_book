5a495a294c2fa3a41444d6a17269743b182198f2
shohei <shoheimoment@gmail.com>
2018-02-25 22:22:47 +0900

    could set curre_user badgets

--- app/helpers/badgets_helper.rb
+++ app/helpers/badgets_helper.rb
@@ -1,23 +1,25 @@
 module BadgetsHelper
   def make_left_categories
-    badgets = Badget.all
     categories = Category.all
     all_categories = Hash.new
     categories.each do |category|
       all_categories[category.kind] = category.id
     end
-
+    left_categories = all_categories
+    unless Badget.find_by(user_id: current_user.id).present?
+      return left_categories
+    end
+    badgets = current_user.badgets
     badget_categories = Array.new
     badgets.each do |badget|
       category = badget.category.kind
       badget_categories << category
     end
 
-    left_categories = all_categories
     badget_categories.each do |category_kind|
       left_categories.delete(category_kind)
     end
-    
+
     return left_categories
   end
 end
