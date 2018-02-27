302f9d93b2c99e82ed566e3e42a2b59af5475245
shohei <shoheimoment@gmail.com>
2018-02-27 21:34:57 +0900

    change arrangement

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -6,5 +6,5 @@ class Expense < ApplicationRecord
   scope :this_month, -> {where('date >= ? AND date <= ?', beginning_of_month, end_of_month)}
   scope :both_f, -> {where(both_flg: false)}
   scope :both_t, -> {where(both_flg: true)}
-  scope :newer, -> {order(date: :desc)}
+  scope :newer, -> {order(date: :desc, created_at: :desc)}
 end
