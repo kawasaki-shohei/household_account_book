e8fb3aa75bd26066b61867b616c8c82e9074477d
shohei <shoheimoment@gmail.com>
2018-03-03 16:15:54 +0900

    add foreign key of user to category table

---
+++ db/migrate/20180303061039_add_user_id_to_categories.rb
@@ -0,0 +1,5 @@
+class AddUserIdToCategories < ActiveRecord::Migration[5.1]
+  def change
+    add_reference :categories, :user, foreign_key: true
+  end
+end
--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20180215124031) do
+ActiveRecord::Schema.define(version: 20180303061039) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -29,6 +29,8 @@ ActiveRecord::Schema.define(version: 20180215124031) do
     t.string "kind"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.bigint "user_id"
+    t.index ["user_id"], name: "index_categories_on_user_id"
   end
 
   create_table "expenses", force: :cascade do |t|
@@ -56,4 +58,5 @@ ActiveRecord::Schema.define(version: 20180215124031) do
 
   add_foreign_key "badgets", "categories"
   add_foreign_key "badgets", "users"
+  add_foreign_key "categories", "users"
 end
