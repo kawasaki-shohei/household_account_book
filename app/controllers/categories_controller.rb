3fab74902cb73e77a5da2363f78ea7cf1260a3a1
shohei <shoheimoment@gmail.com>
2018-03-04 19:28:30 +0900

    update to register common categories

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,11 +1,13 @@
 class CategoriesController < ApplicationController
   before_action :check_logging_in
+  before_action :set_category
   include UsersHelper
 
   def new
-    @categories = current_user.categories
+    @categories = current_user.categories.oneself
     @category = Category.new
     who_is_partner
+    @partner_categories = @partner.categories.oneself
   end
 
   def create
@@ -17,9 +19,22 @@ class CategoriesController < ApplicationController
     end
   end
 
+  def edit
+  end
+
+  def update
+    if params[:name] == "common"
+      @category.update(common: true)
+    end
+    redirect_to new_category_path, notice: "#{@category.kind}を共通のカテゴリーに登録しました！"
+  end
 
   private
   def category_params
     params.require(:category).permit(:kind, :common).merge(user_id: current_user.id)
+  end
+
+  def set_category
+    @category = Category.find(params[:id])
   end
 end
--- app/models/category.rb
+++ app/models/category.rb
@@ -1,6 +1,9 @@
 class Category < ApplicationRecord
   belongs_to :user
   has_one :badget, dependent: :destroy
+  has_many :expenses
+
+  scope :oneself, -> {where(common: false)}
 
 
 end
