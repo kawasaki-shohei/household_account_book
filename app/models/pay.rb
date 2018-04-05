2359f5f4c09d070a6eaacd1e33a81f6cba2dfce8
shohei <shoheimoment@gmail.com>
2018-04-05 21:02:13 +0900

    fix caliculation

--- app/models/pay.rb
+++ app/models/pay.rb
@@ -5,12 +5,8 @@ class Pay < ApplicationRecord
 
   scope :newer, -> {order(date: :desc, created_at: :desc)}
 
-  def self.ones_all_payments(user)
-    user.pays.all.sum(:pamount)
-  end
-
-  def self.total_amount(current_user)
-    current_user.expenses.both_t.sum(:amount) + ones_all_payments(current_user)
+  def self.ones_gross(user)
+    user.expenses.both_t.sum(:amount) + user.pays.all.sum(:pamount)
   end
 
   def self.all_payments(current_user, partner)
@@ -18,16 +14,16 @@ class Pay < ApplicationRecord
   end
 
   def self.balance_of_gross(current_user, partner)
-    my_total_amount = total_amount(current_user)
+    my_gross = ones_gross(current_user)
     my_must_pay = Expense.must_pay(current_user, partner)
-    my_all_pay = ones_all_payments(current_user)
-    if my_total_amount > my_must_pay
-      balance = my_must_pay - my_total_amount
-    elsif my_total_amount < my_must_pay
-      balance = my_must_pay - my_all_pay
-    elsif my_total_amount == my_must_pay
-      balance = 0
-    end
+    balance = my_must_pay - my_gross
+    # if my_gross > my_must_pay
+    #   balance = my_must_pay - my_gross
+    # elsif my_gross < my_must_pay
+    #   balance = my_must_pay - my_gross
+    # elsif my_gross == my_must_pay
+    #   balance = 0
+    # end
     return balance
   end
 
