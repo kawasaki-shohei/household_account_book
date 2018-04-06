bf9e7309f77cbf58b41ec9509333216fa54bed76
shohei <shoheimoment@gmail.com>
2018-04-07 08:19:56 +0900

    making logic of shift months

--- app/models/expense.rb
+++ app/models/expense.rb
@@ -30,13 +30,36 @@ class Expense < ApplicationRecord
     current_user.expenses.both_t.sum(:mypay) + partner.expenses.both_t.sum(:partnerpay)
   end
 
-
-
   def self.expense_in_both_this_month(current_user, partner)
     current_user.expenses.this_month.both_t.sum(:mypay) + partner.expenses.this_month.both_t.sum(:partnerpay) - current_user.expenses.this_month.both_t.sum(:amount)
   end
 
   def self.expense_in_both_last_month(current_user, partner)
     current_user.expenses.last_month.both_t.sum(:mypay) + partner.expenses.last_month.both_t.sum(:partnerpay) - current_user.expenses.last_month.both_t.sum(:amount)
+  end
+
+  def self.past_and_future(beginning_of_month, end_of_month)
+    # 自分一人の出費
+    @current_user_expenses = current_user.expenses.where('date >= ? AND date <= ?', beginning_of_month, end_of_month).both_f.newer
+
+    # 二人の出費の内、自分が払うもの、上記との違いはboth_flgのみ
+    @current_user_expenses_of_both = current_user.expenses.where('date >= ? AND date <= ?', beginning_of_month, end_of_month).both_t.newer
+
+    # 相手が記入した二人の出費の内、自分が払うもの
+    @partner_expenses_of_both = partner.expenses.where('date >= ? AND date <= ?', beginning_of_month, end_of_month).both_t.newer
+    common_variables(@current_user_expenses, @current_user_expenses_of_both, @partner_expenses_of_both)
+  end
+
+  def self.which_month_expense(cnum)
+    if cnum < 0
+      beginning_of_month = Date.today.months_ago(@cnum.abs).beginning_of_month
+      end_of_month = Date.today.months_ago(@cnum.abs).end_of_month
+    elsif cnum == 0
+      redirect_to expenses_path
+    elsif cnum > 0
+      beginning_of_month = Date.today.months_since(@cnum.abs).beginning_of_month
+      end_of_month = Date.today.months_since(@cnum.abs).end_of_month
+    end
+    past_and_future(beginning_of_month, end_of_month)
   end
 end
