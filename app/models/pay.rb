fe13868e578ca6a592102f8cd80101ce00f1c83f
shohei <shoheimoment@gmail.com>
2018-04-07 22:02:32 +0900

    fix caliculation

--- app/models/pay.rb
+++ app/models/pay.rb
@@ -9,6 +9,10 @@ class Pay < ApplicationRecord
     user.expenses.both_t.sum(:amount) + user.pays.all.sum(:pamount)
   end
 
+  def self.ones_all_payment(user)
+    user.pays.all.sum(:pamount)
+  end
+
   def self.all_payments(current_user, partner)
     current_user.pays.or(partner.pays).newer
   end
@@ -16,9 +20,9 @@ class Pay < ApplicationRecord
   def self.balance_of_gross(current_user, partner)
     my_gross = ones_gross(current_user)
     my_must_pay = Expense.must_pay(current_user, partner)
-    balance = my_must_pay - my_gross
+    balance = my_must_pay - my_gross - ones_all_payment(partner)
     # if my_gross > my_must_pay
-    #   balance = my_must_pay - my_gross
+    #   balance = my_must_pay - my_gross 
     # elsif my_gross < my_must_pay
     #   balance = my_must_pay - my_gross
     # elsif my_gross == my_must_pay
