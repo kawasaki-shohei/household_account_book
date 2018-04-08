e0fab5843a6fd8974384d41aed93d82db767a9a7
shohei <shoheimoment@gmail.com>
2018-04-08 23:14:49 +0900

    mistake

--- app/controllers/shift_months_controller.rb
+++ app/controllers/shift_months_controller.rb
@@ -1,37 +1,25 @@
 class ShiftMonthsController < ApplicationController
+  before_action :check_logging_in
+  before_action :check_partner
+
   def past
     @cnum = params[:id].to_i - 1
-    @current_user_expenses = ShiftMonth.ones_expenses(current_user, @cnum)
-    # @current_user_expenses_of_both
-    # @partner_expenses_of_both
-    # @sum
-    # @both_sum
-    # @category_badgets
+    redirect_to expenses_path if @cnum == 0
+    past_and_future(@cnum)
   end
 
   def future
     @cnum = params[:id].to_i + 1
-    if @cnum < 0
-      beginning_of_month = Date.today.months_ago(@cnum.abs).beginning_of_month
-      end_of_month = Date.today.months_ago(@cnum.abs).end_of_month
-    elsif @cnum == 0
-      redirect_to expenses_path
-    elsif @cnum > 0
-      beginning_of_month = Date.today.months_since(@cnum.abs).beginning_of_month
-      end_of_month = Date.today.months_since(@cnum.abs).end_of_month
-    end
-    past_and_future(beginning_of_month, end_of_month)
+    redirect_to expenses_path if @cnum == 0
+    past_and_future(@cnum)
   end
 
-  def past_and_future(beginning_of_month, end_of_month)
-      # 自分一人の出費
-      @current_user_expenses = current_user.expenses.where('date >= ? AND date <= ?', beginning_of_month, end_of_month).both_f.newer
-
-      # 二人の出費の内、自分が払うもの、上記との違いはboth_flgのみ
-      @current_user_expenses_of_both = current_user.expenses.where('date >= ? AND date <= ?', beginning_of_month, end_of_month).both_t.newer
-
-      # 相手が記入した二人の出費の内、自分が払うもの
-      @partner_expenses_of_both = partner.expenses.where('date >= ? AND date <= ?', beginning_of_month, end_of_month).both_t.newer
-      common_variables(@current_user_expenses, @current_user_expenses_of_both, @partner_expenses_of_both)
-    end
+  def past_and_future(cnum)
+    @current_user_expenses = ShiftMonth.ones_expenses(current_user, cnum)
+    @current_user_expenses_of_both = ShiftMonth.ones_expenses_of_both(current_user, cnum)
+    @partner_expenses_of_both = ShiftMonth.ones_expenses_of_both(partner, cnum)
+    @sum = @current_user_expenses.sum(:amount)
+    @both_sum = ShiftMonth.must_pay_this_month(current_user, partner, cnum)
+    @category_badgets = current_user.badgets
+  end
 end
--- app/models/shift_month.rb
+++ app/models/shift_month.rb
@@ -1,7 +1,27 @@
 class ShiftMonth < ApplicationRecord
 
   def self.ones_expenses(user, cnum)
-    user.expenses.which_month(which_month_expense(cnum)).both_f.newer
+    begging_of_one_month = which_month_expense(cnum)[0]
+    end_of_one_month = which_month_expense(cnum)[1]
+    user.expenses.one_month(begging_of_one_month, end_of_one_month).both_f.newer
+  end
+
+  def self.ones_expenses_of_both(user, cnum)
+    begging_of_one_month = which_month_expense(cnum)[0]
+    end_of_one_month = which_month_expense(cnum)[1]
+    user.expenses.one_month(begging_of_one_month, end_of_one_month).both_t.newer
+  end
+
+  def self.partner_expenses_of_both(partner, cnum)
+    begging_of_one_month = which_month_expense(cnum)[0]
+    end_of_one_month = which_month_expense(cnum)[1]
+    partner.expenses.one_month(begging_of_one_month, end_of_one_month).both_t.newer
+  end
+
+  def self.must_pay_this_month(current_user, partner, cnum)
+    begging_of_one_month = which_month_expense(cnum)[0]
+    end_of_one_month = which_month_expense(cnum)[1]
+    current_user.expenses.one_month(begging_of_one_month, end_of_one_month).both_t.sum(:mypay) + partner.expenses.one_month(begging_of_one_month, end_of_one_month).both_t.sum(:partnerpay)
   end
 
   def self.expense_in_both_one_month(current_user, partner, cnum)
@@ -26,8 +46,6 @@ class ShiftMonth < ApplicationRecord
     if cnum < 0
       beginning_of_one_month = Date.today.months_ago(cnum.abs).beginning_of_month
       end_of_one_month = Date.today.months_ago(cnum.abs).end_of_month
-    elsif cnum == 0
-      redirect_to expenses_path
     elsif cnum > 0
       beginning_of_one_month = Date.today.months_since(cnum.abs).beginning_of_month
       end_of_one_month = Date.today.months_since(cnum.abs).end_of_month
