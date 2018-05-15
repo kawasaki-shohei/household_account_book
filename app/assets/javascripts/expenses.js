a46c113a4722259d7d04bfacee80d7634edd383c
shohei <shoheimoment@gmail.com>
2018-05-16 07:31:00 +0900

    add js to calculate automatically

--- app/assets/javascripts/expenses.js
+++ app/assets/javascripts/expenses.js
@@ -1,4 +1,6 @@
 $(function(){
+  $('input:visible').eq(0).focus();
+
   $(document).on('click', 'input#percent_checkbox', function(){
     var chk_status = $('input#percent_checkbox').prop("checked");
     if (chk_status ){
@@ -11,7 +13,7 @@ $(function(){
       $('input#maypay_input, input#partnerpay_input').removeAttr("disabled");
     }
   });
-  
+
   $(document).on('click', 'input#amount_checkbox', function(){
     var chk_status = $('input#amount_checkbox').prop("checked");
     if (chk_status ){
@@ -24,4 +26,47 @@ $(function(){
       $('input#maypay_input, input#partnerpay_input').attr("disabled", "disabled");
     }
   });
+
+  $('input#maypay_input').change(function(){
+    var paid_amount = $('input#paid_amount').val();
+    var mypay = $(this).val();
+    var partnerpay = paid_amount - mypay
+    if (partnerpay < 0){
+      alert('支払金額を超えています。');
+      $('input#maypay_input').focus();
+    }else{
+      $('input#partnerpay_input').val(partnerpay);
+    }
+  });
+
+  $('input#partnerpay_input').change(function(){
+    var paid_amount = $('input#paid_amount').val();
+    var partnerpay = $(this).val();
+    var mypay = paid_amount - partnerpay
+    if (mypay < 0){
+      alert('支払金額を超えています。');
+      $('input#partnerpay_input').focus();
+      return false;
+    }else{
+      $('input#maypay_input').val(mypay);
+    }
+  });
+
+  $('input#paid_amount').change(function(){
+    $('input#maypay_input').val("");
+    $('input#partnerpay_input').val("");
+  });
+
+  // $('form#both_form').on('submit', function(){
+  //   var chk_status = $('input#amount_checkbox').prop("checked");
+  //   var mypay = $('input#maypay_input').val();
+  //   var partnerpay = $('input#partnerpay_input').val();
+  //   var paid_amount = $('input#paid_amount').val();
+  //   var chk_amount = mypay + partnerpay
+  //   if (chk_status && chk_amount != paid_amount){
+  //     alert('支払金額と一致しません');
+  //     $('input#maypay_input').focus();
+  //     return false;
+  //   }
+  // });
 })
