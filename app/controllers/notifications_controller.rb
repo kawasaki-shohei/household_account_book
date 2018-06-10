37615a320832768f74eeecebba3cbfd2d18cfbab
shohei <shoheimoment@gmail.com>
2018-06-10 22:50:30 +0900

    when open notification page, unread notification will be changed to read

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,6 +1,6 @@
 class NotificationsController < ApplicationController
 
   def index
-    @notifications = partner.notifications.where(read_flg: false).order(created_at: :desc)
+    @notifications = partner.notifications.order(created_at: :desc)
   end
 end
