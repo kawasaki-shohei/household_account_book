34f8ff0e67e43def69a5921a8d9be996d214d3c0
shohei <shoheimoment@gmail.com>
2018-06-14 06:31:43 +0900

    do it again because categories were a little strange

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,5 +1,14 @@
 require 'csv'
 
+tables = [Category, Badget, RepeatExpense, Expense]
+tables.each do |t|
+  csv_data = CSV.read("db/#{t.table_name}.csv", headers: true)
+  csv_data.each do |data|
+    t.create!(data.to_hash)
+  end
+end
+
+
 # csv_data = CSV.read('db/categories.csv', headers: true)
 # csv_data.each do |data|
 #   Category.create!(data.to_hash)
@@ -10,15 +19,15 @@ require 'csv'
 #   Badget.create!(data.to_hash)
 # end
 #
-csv_data = CSV.read('db/repeat_expenses.csv', headers: true)
-csv_data.each do |data|
-  RepeatExpense.create!(data.to_hash)
-end
-
-csv_data = CSV.read('db/expenses.csv', headers: true)
-csv_data.each do |data|
-  Expense.create!(data.to_hash)
-end
+# csv_data = CSV.read('db/repeat_expenses.csv', headers: true)
+# csv_data.each do |data|
+#   RepeatExpense.create!(data.to_hash)
+# end
+#
+# csv_data = CSV.read('db/expenses.csv', headers: true)
+# csv_data.each do |data|
+#   Expense.create!(data.to_hash)
+# end
 
 # csv_data = CSV.read('db/pays.csv', headers: true)
 # csv_data.each do |data|
