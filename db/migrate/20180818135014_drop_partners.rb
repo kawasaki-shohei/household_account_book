79a434d09081580fb910e95ef80cc29c1f2110bd
shohei <shoheimoment@gmail.com>
2018-08-21 07:32:51 +0900

    Merge branch 'destroy-partners'

--- db/migrate/20180818135014_drop_partners.rb
+++ db/migrate/20180818135014_drop_partners.rb
@@ -1,4 +1,13 @@
 class DropPartners < ActiveRecord::Migration[5.1]
-  def change
+  def up
+    drop_table :partners
+  end
+
+  def down
+    create_table :partners do |t|
+      t.references :user, foreign_key: true, index: true, unique: true
+      t.references :partner, foreign_key: { to_table: :users }, index: true, unique: true
+      t.timestamps
+    end
   end
 end
