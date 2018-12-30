43f01979497a2f0f8bd73248c412c67fa992d036
shohei <shoheimoment@gmail.com>
2018-12-31 01:37:14 +0900

    add incomes#edit, update, destroy

--- app/controllers/incomes_controller.rb
+++ app/controllers/incomes_controller.rb
@@ -1,4 +1,6 @@
 class IncomesController < ApplicationController
+  before_action :set_income, only: [:edit, :update, :destroy]
+
   def index
     @incomes = current_user.incomes.order(date: :desc)
   end
@@ -12,12 +14,32 @@ class IncomesController < ApplicationController
     if @income.save
       redirect_to incomes_path, notice: "#{@income.amount.to_s(:delimited)}円を収入に追加しました。"
     else
-      render :new
+      render 'new'
     end
+  end
+
+  def edit
+  end
+
+  def update
+    if @income.update(income_params)
+      redirect_to incomes_path, notice: "収入を更新しました。"
+    else
+      render 'edit'
+    end
+  end
+
+  def destroy
+    @income.destroy
+    redirect_to incomes_path, notice: "収入を削除しました。"
   end
 
   private
   def income_params
     params.require(:income).permit(:amount, :date, :memo)
+  end
+
+  def set_income
+    @income = Income.find(params[:id])
   end
 end
