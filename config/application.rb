7e4933cb343d2c471759330d6f4d21f01e7744db
shohei <shoheimoment@gmail.com>
2019-01-05 22:55:10 +0900

    update rails version into 5.2.2 and ruby version 2.5.1

--- config/application.rb
+++ config/application.rb
@@ -9,7 +9,7 @@ Bundler.require(*Rails.groups)
 module HouseholdAccountBook
   class Application < Rails::Application
     # Initialize configuration defaults for originally generated Rails version.
-    config.load_defaults 5.1
+    config.load_defaults 5.2
     config.time_zone = 'Tokyo'
     config.active_record.default_timezone = :local
     config.generators do |g|
