813513b2b89703f0aee6323cce38e76da7f9adc6
shohei <shoheimoment@gmail.com>
2019-02-03 12:44:48 +0900

    monkey patch into string #119
    because output the beginning and end day of the month from text like "2019-01"

---
+++ lib/monkey_patches/string.rb
@@ -0,0 +1,17 @@
+class String
+
+  # @param "2019-01"
+  # @return Dateクラス 月初日
+  def to_beginning_of_month
+    return if self !~ /\d{4}-\d{2}/
+    Date.parse("#{self}-01")
+  end
+
+  # @param "2019-01"
+  # @return Dateクラス 月末日
+  def to_end_of_month
+    return if self !~ /\d{4}-\d{2}/
+    Date.parse("#{self}-01").end_of_month
+  end
+
+end
