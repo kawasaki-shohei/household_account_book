88424c7e8dc8423567494c476a9e295d40041696
shohei <shoheimoment@gmail.com>
2019-02-03 12:40:54 +0900

    update schema and erd by db migration

--- db/schema.rb
+++ db/schema.rb
@@ -80,16 +80,6 @@ ActiveRecord::Schema.define(version: 2019_01_27_102436) do
     t.index ["repeat_expense_id"], name: "index_expenses_on_repeat_expense_id"
   end
 
-  create_table "incomes", force: :cascade do |t|
-    t.bigint "user_id"
-    t.integer "amount"
-    t.date "date"
-    t.string "memo"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-    t.index ["user_id"], name: "index_incomes_on_user_id"
-  end
-
   create_table "notification_messages", force: :cascade do |t|
     t.string "func"
     t.string "act"
@@ -167,7 +157,6 @@ ActiveRecord::Schema.define(version: 2019_01_27_102436) do
   add_foreign_key "deleted_records", "users", column: "deleted_by"
   add_foreign_key "deposits", "users"
   add_foreign_key "expenses", "repeat_expenses"
-  add_foreign_key "incomes", "users"
   add_foreign_key "notifications", "notification_messages"
   add_foreign_key "notifications", "users"
   add_foreign_key "pays", "users"
