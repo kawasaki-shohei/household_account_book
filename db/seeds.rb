b0260a90996675925f481dcc84286c8641a92c15
shohei <shoheimoment@gmail.com>
2019-02-03 16:54:54 +0900

    after seeding data, execute reset pk sequence

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,25 +1,37 @@
 require 'csv'
 
+def delete_all_and_reset_pk_sequence(table)
+  puts "executing - delete all data in #{table} table"
+  table.classify.constantize.all.each { |record| record.delete }
+  one_table_reset_pk_sequence(table)
+end
+
+def one_table_reset_pk_sequence(table)
+  puts "executing - reset_pk_sequence! #{table} table"
+  ActiveRecord::Base.connection.reset_pk_sequence!(table)
+end
+
 # tables = [Category, RepeatExpense, Expense]
 puts "starding - #{__FILE__}"
 tables = [Category, Badget, RepeatExpense, Expense, Pay, Want, NotificationMessage, Notification, DeletedRecord]
 
-tables.map(&:table_name).reverse.each do |t|
-  puts "executing - delete all data in #{t} table"
-  t.classify.constantize.all.each { |record| record.delete }
-  puts "executing - reset_pk_sequence! #{t} table"
-  ActiveRecord::Base.connection.reset_pk_sequence!(t)
+tables.map(&:table_name).reverse.each do |table|
+  delete_all_and_reset_pk_sequence(table)
 end
 
-tables.each do |t|
+tables.each do |table|
   puts "-------------------------------------"
-  puts "seeding - table: #{t}"
+  puts "seeding - table: #{table}"
   puts "-------------------------------------"
-  csv_data = CSV.read("db/csv/#{t.table_name}.csv", headers: true)
+  csv_data = CSV.read("db/csv/#{table.table_name}.csv", headers: true)
   csv_data.each do |data|
     puts "seeding - data: #{data} "
-    t.create!(data.to_hash)
+    table.create!(data.to_hash)
   end
+end
+
+tables.map(&:table_name).reverse.each do |table|
+  one_table_reset_pk_sequence(table)
 end
 
 
