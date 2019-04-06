5f00ae323f974064361b4ea3e523ddcb48c87b96
shohei <shoheimoment@gmail.com>
2019-04-06 09:09:06 +0900

    rename pamount

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_02_03_054539) do
+ActiveRecord::Schema.define(version: 2019_04_06_000013) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -110,7 +110,7 @@ ActiveRecord::Schema.define(version: 2019_02_03_054539) do
   end
 
   create_table "pays", force: :cascade do |t|
-    t.integer "pamount"
+    t.integer "amount"
     t.bigint "user_id"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,3 +1,34 @@
+# ## Schema Information
+#
+# Table name: `users`
+#
+# ### Columns
+#
+# Name                   | Type               | Attributes
+# ---------------------- | ------------------ | ---------------------------
+# **`id`**               | `bigint(8)`        | `not null, primary key`
+# **`allow_share_own`**  | `boolean`          | `default(FALSE)`
+# **`email`**            | `string`           |
+# **`name`**             | `string`           |
+# **`password_digest`**  | `string`           |
+# **`sys_admin`**        | `boolean`          | `default(FALSE)`
+# **`created_at`**       | `datetime`         | `not null`
+# **`updated_at`**       | `datetime`         | `not null`
+# **`partner_id`**       | `bigint(8)`        |
+#
+# ### Indexes
+#
+# * `index_users_on_email` (_unique_):
+#     * **`email`**
+# * `index_users_on_partner_id` (_unique_):
+#     * **`partner_id`**
+#
+# ### Foreign Keys
+#
+# * `fk_rails_...`:
+#     * **`partner_id => users.id`**
+#
+
 FactoryBot.define do
   # factory :users do
   #   email "user@gmail.com"
