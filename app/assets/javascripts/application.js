3bac20556c9d312839510e0f9d624bd9cb028271
shohei <shoheimoment@gmail.com>
2019-04-08 22:59:26 +0900

    sortableのtouch対応 #126

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -15,4 +15,5 @@
 //= require bootstrap/dist/js/bootstrap.min
 //= require admin-lte/dist/js/adminlte.min
 //= require admin-lte/plugins/jQueryUI/jquery-ui.min
+//= require jquery-ui-touch-punch/jquery.ui.touch-punch.min
 //= require_tree .
