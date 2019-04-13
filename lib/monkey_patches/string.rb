bfa24808ad91c42d1206a90bb0ab2227954ea8c9
shohei <shoheimoment@gmail.com>
2019-04-13 18:12:54 +0900

    paramsで受け取るyear_monthを検証するメソッドをStringに追加 #126

--- lib/monkey_patches/string.rb
+++ lib/monkey_patches/string.rb
@@ -3,14 +3,14 @@ class String
   # @param "2019-01"
   # @return Dateクラス 月初日
   def to_beginning_of_month
-    return unless self.match?(/^\d{4}-\d{2}$/)
+    return unless self.match?(year_month_regex)
     Date.parse("#{self}-01")
   end
 
   # @param "2019-01"
   # @return Dateクラス 月末日
   def to_end_of_month
-    return unless self.match?(/^\d{4}-\d{2}$/)
+    return unless self.match?(year_month_regex)
     Date.parse("#{self}-01").end_of_month
   end
 
@@ -18,8 +18,21 @@ class String
   # @param "2019-01"
   # @return Boolean
   def is_after_next_month?
-    return unless self.match?(/^\d{4}-\d{2}$/)
+    return unless self.match?(year_month_regex)
     self.to_beginning_of_month > Date.current.beginning_of_month
+  end
+
+  def is_valid_year_month?
+    self.match?(year_month_regex)
+  end
+
+  def is_invalid_year_month?
+    !is_valid_year_month?
+  end
+
+  private
+  def year_month_regex
+    /^\d{4}-\d{2}$/
   end
 
 end
