914552005dc6a4440c1ad954de4abc3ad8531def
shohei <shoheimoment@gmail.com>
2019-04-27 10:45:56 +0900

    パートナーがいないときのヘッダーの通知を修正 #126

--- app/controllers/notifications_controller.rb
+++ app/controllers/notifications_controller.rb
@@ -1,5 +1,5 @@
 class NotificationsController < ApplicationController
-  skip_before_action :count_notifications
+  skip_before_action :count_notifications, raise: false
 
   def index
     @notifications = @partner.notifications.includes(:user, :notification_message).order(created_at: :desc).page(params[:page])
