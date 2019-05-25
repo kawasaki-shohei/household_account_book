8147f89713f0cb9e0169d6f68e66e22cbebcbd86
shohei <shoheimoment@gmail.com>
2019-05-25 13:33:13 +0900

    とりあえずreactの導入が成功

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,15 @@
 module ApplicationHelper
 
+  def render_react_component(*args, &block)
+    content_for :webpacker_assets do
+      assets = []
+      assets << javascript_pack_tag('application')
+      assets << stylesheet_pack_tag('application')
+      assets.join("\n").html_safe
+    end
+    react_component(*args, &block)
+  end
+
   def submit_btn_letters
     case action_name
     when 'new', 'withdraw'
---
+++ app/javascript/components/Calculator.js
@@ -0,0 +1,13 @@
+import React from "react"
+import PropTypes from "prop-types"
+class Calculator extends React.Component {
+  render () {
+    return (
+      <React.Fragment>
+        <div id="hoge">hello calculator</div>
+      </React.Fragment>
+    );
+  }
+}
+
+export default Calculator
