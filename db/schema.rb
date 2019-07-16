0e1f52c9bf2c89b71e7efd83dcd79dc996116745
shohei <shoheimoment@gmail.com>
2019-07-17 07:46:44 +0900

    管理者テーブルとシードを作成 #126

--- db/schema.rb
+++ db/schema.rb
@@ -10,10 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2019_07_15_100807) do
+ActiveRecord::Schema.define(version: 2019_07_16_214300) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
+
+  create_table "admins", force: :cascade do |t|
+    t.string "name", null: false
+    t.string "email", null: false
+    t.string "password_digest", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["email"], name: "index_admins_on_email", unique: true
+  end
 
   create_table "balances", force: :cascade do |t|
     t.bigint "user_id"
