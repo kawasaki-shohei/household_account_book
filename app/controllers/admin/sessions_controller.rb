12b6f1a656906a3bfc2c84f24812e507253f22c5
shohei <shoheimoment@gmail.com>
2019-07-20 08:55:48 +0900

    管理者ログインとログアウトを実装 #126

--- app/controllers/admin/sessions_controller.rb
+++ app/controllers/admin/sessions_controller.rb
@@ -1,16 +1,26 @@
 class Admin::SessionsController < AdminController
-  skip_before_action :check_logging_in
+  skip_before_action :check_logging_in, only: [:new, :create]
 
   def new
     if logged_in?
-      redirect_to admin_users_path
+      redirect_to admin_top_path
     end
   end
 
   def create
+    admin = Admin.find_by(email: params[:session][:email])
+    if admin && admin.authenticate(params[:session][:password])
+      session[:admin_id] = admin.id
+      redirect_to admin_top_path
+    else
+      redirect_to admin_login_path, alert: 'メールアドレスまたはパスワードが違います。'
+    end
   end
 
   def destroy
+    session.delete(:admin_id)
+    flash[:notice] = 'ログアウトしました'
+    redirect_to admin_login_path
   end
 
 end
