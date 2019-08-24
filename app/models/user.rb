68c9ee38c6721084cff4bdfe209c430eba9c03cc
shohei <shoheimoment@gmail.com>
2019-08-24 19:00:16 +0900

    新規登録画面の文言をlocalファイルを使ったものに置き換えた

--- app/models/user.rb
+++ app/models/user.rb
@@ -53,7 +53,7 @@ class User < ApplicationRecord
 
   def password_complexity
     return if password.blank? || password =~ /^(?=.*?[A-Z])(?=.*?[a-z])(?=.*?[0-9])(?=.*?[#?!@$%^&*-]).{8,70}$/
-    errors.add :password, "パスワードの強度が不足しています。パスワードの長さは8〜70文字とし、大文字と小文字と数字と特殊文字をそれぞれ1文字以上含める必要があります。"
+    errors.add(:password, I18n.t('user.validation.weak_password'))
   end
 
   def check_valid_partner
