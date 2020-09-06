50fcd4f99340bc70c9ce066e9ae080fab6b2c125
shohei <shoheimoment@gmail.com>
2020-09-06 13:05:08 +0900

    デモ機能を修正
    LPからdemo#createへpostして、無理やり行っていたが、
    デモ起動ページを作成し、そのページからpostするように変更

--- app/assets/javascripts/demo.js
+++ app/assets/javascripts/demo.js
@@ -1,3 +1,8 @@
 window.onload = () => {
   document.querySelector('#demo-btn').click();
+  document.querySelectorAll('a').forEach( (aTag) => {
+    aTag.addEventListener('click', (event) => {
+      event.preventDefault();
+    })
+  })
 };
